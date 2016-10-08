d109ccd6893b1d77dd393e21c0ce95a6d4c3277a
ArturGrigio <fsastudent@yahoo.com>
2016-10-07 19:00:10 -0700

    finished the simplified version of Image To Video

--- imageToVideo.php
+++ imageToVideo.php
@@ -3,34 +3,44 @@
 $pathToImg = "./images/";
 $pathToMusic = "./music/";
 $music = "jass.mp3";
-$postfix = "";
+$agent_name = "artur grigio";
+$agent_phone = "(626) 555-1234";
+$agent_email = "test@yahoo.com";
+$font = 15;
+
 $transition = 1; // In Seconds
 $holdFrame = 2; // Hold the frame in Seconds
 $fps = 30; // Frames per second
 
 $images = glob("$pathToImg*.jpg");
-foreach ($images as $img1)
-    printf("globbed image %s\n", $img1);
 
-exec("convert $pathToImg*.jpg -morph ".$transition*$fps." $pathToImg%08d$postfix.jpg");
-$morphs = glob("$pathToImg*$postfix.jpg");
-foreach($morphs as $m)
-    printf("created morph: %s\n", $m);
+exec("convert $pathToImg*.jpg -morph ".$transition*$fps." $pathToImg%07d.jpg");
+$morphs = glob($pathToImg."0*.jpg");
 
 $counter = 0;
 foreach($morphs as $key=>$morph) {
     if($key % ($transition*$fps+1) == 0) {
         for($k=0; $k<$holdFrame*$fps; $k++) {
-            printf(copy($morph, $pathToImg.str_pad($counter++, 8, '0', STR_PAD_LEFT)."$postfix.jpg"));
+            copy($morph, $pathToImg.str_pad($counter++, 8, '0', STR_PAD_LEFT).".jpg");
         }
     }
-    rename($morph, $pathToImg.str_pad($counter++, 8, '0', STR_PAD_LEFT)."$postfix.jpg");
+    rename($morph, $pathToImg.str_pad($counter++, 8, '0', STR_PAD_LEFT).".jpg");
 }
-exec("ffmpeg -r ".$transition*$fps." -i ./images/%08d$postfix.jpg -i $pathToMusic$music -t ".count($images)*($transition+$holdFrame)." -c:v mpeg4 out.mp4");
-exec('convert -size 500x500 xc:none -font Arial -pointsize 20 -gravity NorthWest -draw "text 0,0 \'Artur Grigio\'" -draw "text 0,24 \'test\'" watermarkfile.png');
-exec('ffmpeg -i out.mp4 -i watermarkfile.png -filter_complex "overlay=10:15" finished.mp4');
+// Creating the original OUT video
+exec("ffmpeg -r ".$transition*$fps." -i ".$pathToImg."%08d.jpg -i ".$pathToMusic.$music." -t ".count($images)*($transition+$holdFrame)." -c:v mpeg4 out.mp4");
+// Adding Agent Info
+if($agent_phone && $agent_email)
+    exec('convert -size 500x500 xc:none -font Arial -pointsize '. $font .' -gravity NorthWest -draw "text 0,0 \''.$agent_name.'\'" -draw "text 0,'. ($font+1) .' \'Phone: '.$agent_phone.'\'" -draw "text 0,'. 2*($font+1) .' \'E-Mail: '.$agent_email.'\'" watermarkfile.png');
+else if($agent_phone)
+    exec('convert -size 500x500 xc:none -font Arial -pointsize '. $font .' -gravity NorthWest -draw "text 0,0 \''.$agent_name.'\'" -draw "text 0,'. ($font+1) .' \'Phone: '.$agent_phone.'\'" watermarkfile.png');
+else
+    exec('convert -size 500x500 xc:none -font Arial -pointsize '. $font .' -gravity NorthWest -draw "text 0,0 \''.$agent_name.'\'" watermarkfile.png');
 
-exec("rm $pathToImg"."0*$postfix.jpg");
+// Placing the watermark
+exec('ffmpeg -i out.mp4 -i watermarkfile.png -filter_complex "overlay=7:7" finished.mp4');
+
+// Deleting files
+exec("rm $pathToImg"."0*.jpg");
 exec('rm ./watermarkfile.png');
 exec('rm ./out.mp4');
 
