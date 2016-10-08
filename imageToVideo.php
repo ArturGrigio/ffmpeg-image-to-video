9bf028b619dea412d23e3f8bcf3032a6de324662
shootingla <shootingla@cloud>
2016-10-08 03:54:52 -0400

    half way through the API

--- imageToVideo.php
+++ imageToVideo.php
@@ -1,15 +1,15 @@
 <?php
 $musicUrl = "https://listingzen.com/music/Piano.mp3";
-$urls = array( "/prop0/001.jpg", "/prop0/002.jpg", "/prop0/003.jpg", "/prop2/prop2-006.jpg", "/prop4657/4337MarinaCityDrPH43Reshoot-001.jpg" );
+$urls = array( "/prop0/001.jpg", "/prop2/prop2-006.jpg", "/prop4657/4337MarinaCityDrPH43Reshoot-001.jpg" );
 $profile_image = "/agent2/agent1469316620.jpg";
 $property = "711 E. Windsor Rd. ";
-$line_1 = "Artur Grigio";
-$line_2 = "(626) 555-1234";
-$line_3 = "test@yahoo.com";
+$line_1 = htmlspecialchars($_POST["line_1"]);
+$line_2 = htmlspecialchars($_POST["line_2"]);
+$line_3 = htmlspecialchars($_POST["line_3"]);
 
 $out = preg_replace('/[^a-zA-Z0-9.\']/', '_', $property);
 $out = substr(str_replace(["'", " ", "."], '', $out),0, 20)."-".time()%1000000;
-$pathToImg = "/home/shootingla/var/www/html/images/imageToVideo/".$out."/";
+$pathToImg = "/home/shootingla/var/www/html/images/ffmpeg-image-to-video/".$out."/";
 $pathToLZ = "/home/shootingla/var/www/html/images/ListingZen";
 
 $font = 15; // Font Size (Can't change without changing the Watermark Image Size
@@ -46,19 +46,24 @@ foreach($morphs as $key=>$morph) {
 
 // Adding Agent Info
 if($line_2 && $line_3)
-    exec('convert -size 300x82 xc:"rgba(255,255,255,0.65)" -font Helvetica -pointsize '. $font .' -gravity North -draw "text 0,'. $font .' \''.$line_1.'\'" -draw "text 0,'. 2*($font+1) .' \''.$line_2.'\'" -draw "text 0,'. 3*($font+1) .' \''.$line_3.'\'" '.$pathToImg.'watermarkfile.png');
+    exec('convert -size 300x85 xc:"rgba(255,255,255,0.65)" -font Helvetica -pointsize '. $font .' -gravity North -draw "text 0,'. $font+2 .' \''.$line_1.'\'" -draw "text 0,'. 2*($font+1) .' \''.$line_2.'\'" -draw "text 0,'. 3*($font+1) .' \''.$line_3.'\'" '.$pathToImg.'watermarkfile.png');
 else if($line_2)
-    exec('convert -size 300x65 xc:"rgba(255,255,255,0.65)" -font Arial -pointsize '. $font .' -gravity North -draw "text 0,'. $font .' \''.$line_1.'\'" -draw "text 0,'. 2*($font+1) .' \''.$line_2.'\'" '.$pathToImg.'watermarkfile.png');
+    exec('convert -size 300x85 xc:"rgba(255,255,255,0.65)" -font Arial -pointsize '. $font .' -gravity North -draw "text 0,'. 2*($font-2) .' \''.$line_1.'\'" -draw "text 0,'. 2*($font+8) .' \''.$line_2.'\'" '.$pathToImg.'watermarkfile.png');
 else
-    exec('convert -size 300x40 xc:"rgba(255,255,255,0.65)" -font Arial -pointsize '. $font .' -gravity Center -draw "text 0,0 \''.$line_1.'\'" '.$pathToImg.'watermarkfile.png');
+    exec('convert -size 300x85 xc:"rgba(255,255,255,0.65)" -font Arial -pointsize '. $font .' -gravity Center -draw "text 0,0 \''.$line_1.'\'" '.$pathToImg.'watermarkfile.png');
 
 
 // Creating the original OUT video
 exec("ffmpeg -r ".$transition*$fps." -i ".$pathToImg."%08d.jpg -i ".$pathToImg."music.mp3 -t ".count($images)*($transition+$holdFrame)." -vf scale=600:400 -pix_fmt yuv420p -vcodec libx264 -strict -2 ".$pathToImg.$out.".mp4");
 
 // Placing the watermark
-exec('ffmpeg -i '.$pathToImg.$out.'.mp4 -i '.$pathToImg.'watermarkfile.png -filter_complex "overlay=(main_w-overlay_w)-7:main_h-overlay_h-7" -strict -2 '.$out.'.mp4');
-$retVid = $out.".mp4";
+if($profile_image) {
+    exec('ffmpeg -i '.$pathToImg.$out.'.mp4 -i '.$pathToImg.'watermarkfile.png -filter_complex "overlay=92:main_h-92" -strict -2 '.$out.'.mp4');
+    $retVid = $out.".mp4";
+} else {
+    exec('ffmpeg -i '.$pathToImg.$out.'.mp4 -i '.$pathToImg.'watermarkfile.png -filter_complex "overlay=(main_w-overlay_w)-7:main_h-overlay_h-7" -strict -2 '.$out.'.mp4');
+    $retVid = $out.".mp4";
+}
 // Downloading The Agent Image
 if($profile_image) {
     $profile = $pathToImg . basename($profile_image);
